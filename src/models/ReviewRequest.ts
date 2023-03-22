39ea4fa5ac8bdfde44d8e47d57e5559fce1b0402
Jiwoon <kfromh0136@Naver.com>
2023-03-22 14:33:21 +0900

    프론트엔드 레포지토리 분리
    배포를 대비해 프론트엔드 디렉토리를 별도의 깃허브 레포지토리로 분리했음.

---
+++ src/models/ReviewRequest.ts
@@ -0,0 +1,12 @@
+class ReviewRequest {
+  rating: number;
+  bookId: number;
+  reviewDescription?: string;
+
+  constructor(rating: number, bookId: number, reviewDescription: string) {
+    this.rating = rating;
+    this.bookId = bookId;
+    this.reviewDescription = reviewDescription;
+  }
+}
+export default ReviewRequest;
